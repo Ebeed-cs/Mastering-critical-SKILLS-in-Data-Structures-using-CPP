9ce7a2cb54dfdf64e0ba73feaeb1431d949f7f5e
Ebeed-cs <Ebeed-cs@users.noreply.github.com>
2025-11-26 14:14:07 +0200

    MOD right/left rotate& ADD DeletingAPosition

--- 02-Vector/Medium Challenges/Deleting a position.cpp
+++ 02-Vector/Medium Challenges/Deleting a position.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 #include <cassert>
 
-class Vector
-{
+class Vector {
 private:
-    int *ptr = nullptr;
+    int* ptr = nullptr;
     int captcity{};
     int size{};
-
 public:
     Vector() = default;
-    Vector(int s) : captcity(s * 2)
-    {
+    Vector(int s) : captcity(s * 2) {
         ptr = new int[captcity];
     }
 
-    void print()
-    {
-        if (size == 0)
-        {
+    void print() {
+        if (size == 0) {
             std::cout << "it's empty" << "\n";
             return;
         }
@@ -27,31 +22,26 @@ public:
                 std::cout << ptr[i] << " ";
     }
 
-    void resize()
-    {
+    void resize() {
         captcity *= 2;
-        int *ptr2 = new int[captcity]{};
-        for (int i = 0; i < size; ++i)
-            ptr2[i] = ptr[i];
+        int* ptr2 = new int[captcity] {};
+        for (int i = 0; i < size; ++i) ptr2[i] = ptr[i];
         Clear();
         std::swap(ptr, ptr2);
     }
 
-    void push_back(int val)
-    {
-        if (size == captcity)
-            resize();
+
+    void push_back(int val) {
+        if (size == captcity) resize();
         ptr[size++] = val;
     }
 
-    int &operator[](int ind)
-    {
+    int& operator[](int ind) {
         assert(ind >= 0 && ind < size);
         return ptr[ind];
     }
 
-    void insert(int val, int ind)
-    {
+    void insert(int val, int ind) {
         assert(ind >= 0 && ind < size);
         for (int i = size; i > ind; --i)
             ptr[i] = ptr[i - 1];
@@ -59,27 +49,36 @@ public:
         size++;
     }
 
-    void rightRotation()
-    {
+    void rightRotation() {
         insert(ptr[size - 1], 0);
         size--;
     }
 
-    void Clear()
-    {
+    void left_rotate() {
+        int temp = ptr[0];
+        for (int i = 1; i < size; ++i)
+            ptr[i - 1] = ptr[i];
+        ptr[size - 1] = temp;
+    }
+
+    void right_rotate(int times) {
+        times %= size - 1;
+        for (int i = 0; i < times; ++i)
+            rightRotation();
+    }
+
+    void Clear() {
         if (ptr != nullptr)
             delete[] ptr;
         ptr = nullptr;
     }
 
-    ~Vector()
-    {
+    ~Vector() {
         Clear();
     }
 };
 
-int main()
-{
+int main() {
     Vector v(10);
     v.push_back(0);
     v.push_back(1);
@@ -88,8 +87,8 @@ int main()
     v.push_back(4);
     v.print();
 
-    std::cout << "\nAfter roration right \n";
-    v.rightRotation();
+    std::cout << "\nAfter right rotation times \n";
+    v.right_rotate(2000000000);
 
     v.print();
 
--- 02-Vector/Medium Challenges/Left rotation.cpp
+++ 02-Vector/Medium Challenges/Left rotation.cpp
@@ -1,15 +1,3 @@
-/*
-    Left rotation
-● Consider our Vector class. Add the member function: void left_rotate()
-● The function rotates the whole array 1 step to the left
-○ However, in this case, the leftmost element will be 'rotated' around to the back of the array!
-● Example
-● Assume the array content is: 0 1 2 3 4
-● After a left rotation, it will be: 1 2 3 4 0
-○ Notice how the 0 has 'rotated' to the tail of the array after applying left_rotate()
-● Ensure you avoid expanding the array's capacity
-*/
-
 #include <iostream>
 #include <cassert>
 
