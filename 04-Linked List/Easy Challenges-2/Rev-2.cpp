d31b561817f8f384f6545c9b0d04cefc38d4fd1f
Ebeed-cs <Ebeed-cs@users.noreply.github.com>
2025-12-05 02:05:59 +0200

    ADD reverse

---
+++ 04-Linked List/Easy Challenges-2/Rev-2.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+struct Node {
+    int data{ };
+    Node* next{ };
+    Node(int data) : data(data) {}
+    ~Node() {
+        std::cout << "Destroy value: " << data << " at address " << this << "\n";
+    }
+};
+
+class LinkedList {
+private:
+    Node* head{};
+    Node* tail{};
+    int length{};
+public:
+    LinkedList() = default;
+
+    void insert(int val) {
+        Node* node = new Node(val);
+        length++;
+        if (head == nullptr) {
+            head = tail = node;
+            return;
+        }
+        tail->next = node;
+        tail = node;
+    }
+
+    void debug_verify_data_integrity() {
+        if (length == 0) {
+            assert(head == nullptr);
+            assert(tail == nullptr);
+        }
+        else {
+            assert(head != nullptr);
+            assert(tail != nullptr);
+            if (length == 1)
+                assert(head == tail);
+            else
+                assert(head != tail);
+            assert(!tail->next);
+        }
+        int len = 0;
+        for (Node* cur = head; cur; cur = cur->next, len++)
+            assert(len < 10000);	// Consider infinite cycle?
+        assert(length == len);
+        // assert(length == (int)debug_data.size());
+    }
+
+    void print() {
+        Node* temp_head = head;
+        while (temp_head != nullptr) {
+            std::cout << temp_head->data << " ";
+            temp_head = temp_head->next;
+        }
+        std::cout << "\n";
+        std::cout << "the current length is :" << length << "\n";
+    }
+
+    LinkedList(const LinkedList&) = delete;
+    LinkedList& operator=(const LinkedList&) = delete;
+
+    void insert_front(int val) {
+        length++;
+        Node* node = new Node(val);
+        if (head == nullptr) {
+            head = tail = node;
+            return;
+        }
+        node->next = head;
+        head = node;
+    }
+
+    void reverse() { // o(1) memory , o(n) time
+        if (length <= 1) return;
+        tail = head;
+        Node* prev = head;
+        head = head->next;
+        while (head)
+        {
+            Node* after = head->next;
+            head->next = prev;
+            prev = head;
+            head = after;
+        }
+        head = prev;
+        tail->next = nullptr;
+
+    }
+
+    ~LinkedList() {
+        while (head != nullptr) {
+            Node* temp_head = head;
+            head = head->next;
+            delete temp_head;
+        }
+        tail = nullptr;
+    }
+};
+
+int main() {
+    LinkedList list1;
+
+    list1.insert(1);
+    list1.insert(2);
+    list1.reverse();
+    list1.insert(3);
+    list1.insert(4);
+    list1.insert(5);
+    list1.print();
+
+    // must see it, otherwise RTE
+    std::cout << "\n\nNO RTE\n";
+    return 0;
+}
