9061feafdc4bc09ecf9941d7bb73becc582945a6
Ebeed-cs <Ebeed-cs@users.noreply.github.com>
2025-12-10 15:37:18 +0200

    ADD Problem4

---
+++ 04-Linked List/Medium Challenges/Problem4.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+struct Node {
+    int data{ };
+    Node* next{ };
+    Node(int data) : data(data) {}
+    ~Node() {
+        std::cout << "Destroy value: " << data << " at address " << this << "\n";
+    }
+};
+
+class LinkedList {
+private:
+    Node* head{};
+    Node* tail{};
+    int length{};
+public:
+    LinkedList() = default;
+
+    void insert(int val) {
+        Node* node = new Node(val);
+        length++;
+        if (head == nullptr) {
+            head = tail = node;
+            return;
+        }
+        tail->next = node;
+        tail = node;
+    }
+
+    void debug_verify_data_integrity() {
+        if (length == 0) {
+            assert(head == nullptr);
+            assert(tail == nullptr);
+        }
+        else {
+            assert(head != nullptr);
+            assert(tail != nullptr);
+            if (length == 1)
+                assert(head == tail);
+            else
+                assert(head != tail);
+            assert(!tail->next);
+        }
+        int len = 0;
+        for (Node* cur = head; cur; cur = cur->next, len++)
+            assert(len < 10000);	// Consider infinite cycle?
+        assert(length == len);
+        // assert(length == (int)debug_data.size());
+    }
+
+    void print() {
+        Node* temp_head = head;
+        while (temp_head != nullptr) {
+            std::cout << temp_head->data << " ";
+            temp_head = temp_head->next;
+        }
+        std::cout << "\n";
+        std::cout << "the current length is :" << length << "\n";
+    }
+
+    LinkedList(const LinkedList&) = delete;
+    LinkedList& operator=(const LinkedList&) = delete;
+
+    void insert_front(int val) {
+        length++;
+        Node* node = new Node(val);
+        if (head == nullptr) {
+            head = tail = node;
+            return;
+        }
+        node->next = head;
+        head = node;
+    }
+
+    void delete_node(Node* node) {
+        --length;
+        delete node;
+    }
+
+    void delete_next_node(Node* node) {
+        // Delete the next of the current node
+        // Handle if next is tail case
+        assert(node);
+
+        Node* to_delete = node->next;
+        bool is_tail = to_delete == tail;
+
+        // node->next in middle to delete
+        node->next = node->next->next;
+
+        delete_node(to_delete);
+        if (is_tail)
+            tail = node;
+    }
+
+    void Rem2(int key) {  // O(n) time & o(1) memory
+        if (!length) return;
+        length--;
+        Node* prev = head;
+        for (Node* cur = head; cur->next; cur = cur->next)
+            if (cur->next->data == key)
+                prev = cur;
+        if (prev == head && prev->data == key) { // is the  head node is the target ?
+            if (head == tail) tail = tail->next;
+            head = head->next;
+            delete prev;
+        }
+        else if (prev->next->data == key) // is the next node the target ?
+            delete_next_node(prev);
+        length++; // then the node key not found -> then reset the length to it's orginal value
+    }
+
+    ~LinkedList() {
+        while (head != nullptr) {
+            Node* temp_head = head;
+            head = head->next;
+            delete temp_head;
+        }
+        tail = nullptr;
+    }
+};
+
+int main() {
+    LinkedList list1;
+
+    list1.insert(1);
+    list1.insert(2);
+    list1.insert(3);
+    list1.insert(4);
+
+    list1.Rem2(7);
+
+    list1.print();
+
+
+    // must see it, otherwise RTE
+    std::cout << "\n\nNO RTE\n";
+    return 0;
+}
