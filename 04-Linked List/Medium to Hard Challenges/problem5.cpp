aac239b8f1a50deb1612c42336e3da715970bc49
Ebeed-cs <Ebeed-cs@users.noreply.github.com>
2025-12-20 13:08:22 +0200

    ADD 04-Linked List/Medium to Hard Challenges/problem5

---
+++ 04-Linked List/Medium to Hard Challenges/problem5.cpp
@@ -0,0 +1,201 @@
+#include <iostream>
+#include <cassert>
+#include <climits>
+#include <tuple>
+
+#include <vector>		// for debug
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+struct Node {
+    int data{ };
+    Node* next{ };
+    Node(int data) :
+        data(data) {
+    }
+
+    ~Node() {
+        cout << "Destroy value: " << data << "\n";
+    }
+};
+
+class LinkedList {
+private:
+    Node* tail{ };
+    int length = 0;	// let's maintain how many nodes
+
+    vector<Node*> debug_data;	// add/remove nodes you use
+
+public:
+    Node* head{ };
+
+    // Below 2 deletes prevent copy and assign to avoid this mistake
+    LinkedList() {
+    }
+    LinkedList(const LinkedList&) = delete;
+    LinkedList& operator=(const LinkedList& another) = delete;
+
+    void debug_print_address() {
+        for (Node* cur = head; cur; cur = cur->next)
+            cout << cur << "," << cur->data << "\t";
+        cout << "\n";
+    }
+
+    void debug_print_node(Node* node, bool is_seperate = false) {
+        if (is_seperate)
+            cout << "Sep: ";
+        if (node == nullptr) {
+            cout << "nullptr\n";
+            return;
+        }
+        cout << node->data << " ";
+        if (node->next == nullptr)
+            cout << "X ";
+        else
+            cout << node->next->data << " ";
+
+        if (node == head)
+            cout << "head\n";
+        else if (node == tail)
+            cout << "tail\n";
+        else
+            cout << "\n";
+    }
+    void debug_print_list(string msg = "") {
+        if (msg != "")
+            cout << msg << "\n";
+        for (int i = 0; i < (int)debug_data.size(); ++i)
+            debug_print_node(debug_data[i]);
+        cout << "************\n" << flush;
+    }
+
+    void debug_add_node(Node* node) {
+        debug_data.push_back(node);
+    }
+    void debug_remove_node(Node* node) {
+        auto it = std::find(debug_data.begin(), debug_data.end(), node);
+        if (it == debug_data.end())
+            cout << "Node does not exist\n";
+        else
+            debug_data.erase(it);
+    }
+
+    string debug_to_string() {
+        if (length == 0)
+            return "";
+        ostringstream oss;
+        for (Node* cur = head; cur; cur = cur->next) {
+            oss << cur->data;
+            if (cur->next)
+                oss << " ";
+        }
+        return oss.str();
+    }
+
+    void debug_verify_data_integrity() {
+        if (length == 0) {
+            assert(head == nullptr);
+            assert(tail == nullptr);
+        }
+        else {
+            assert(head != nullptr);
+            assert(tail != nullptr);
+
+            if (length == 1)
+                assert(head == tail);
+            else
+                assert(head != tail);
+            assert(!tail->next);
+        }
+
+        int len = 0;
+        for (Node* cur = head; cur; cur = cur->next, len++)
+            assert(len < 10000);	// Consider infinite cycle?
+
+        assert(length == len);
+        //assert(length == (int )debug_data.size());
+    }
+    ////////////////////////////////////////////////////////////
+
+    void print() {
+        for (Node* cur = head; cur; cur = cur->next)
+            cout << cur->data << " ";
+        cout << "\n";
+    }
+
+    void insert_end(int value) {
+        Node* item = new Node(value);
+        if (!head)
+            head = tail = item;
+        else
+            tail->next = item, tail = item;
+        ++length;
+
+        debug_add_node(item);
+    }
+
+    void delete_node(Node* node) {
+        debug_remove_node(node);
+        --length;
+        delete node;
+    }
+
+    void insert_after(Node* src, Node* target) {
+        // Given node src, link target after it with my list before/after
+        assert(src && target);
+        target->next = src->next;
+        src->next = target;
+        debug_add_node(target);
+        ++length;
+    }
+
+    void delete_next_node(Node* node) {
+        // Delete the next of the current node
+        // Handle if next is tail case
+        assert(node);
+
+        Node* to_delete = node->next;
+        bool is_tail = to_delete == tail;
+
+        // node->next in middle to delete
+        node->next = node->next->next;
+
+        delete_node(to_delete);
+        if (is_tail)
+            tail = node;
+    }
+    void add_node(Node* node) {
+        debug_add_node(node);
+        ++length;
+    }
+
+    void insert_front(int value) {
+        Node* item = new Node(value);
+        add_node(item);
+
+        item->next = head;
+        head = item;
+
+        if (length == 1)
+            tail = head;
+    }
+    void delete_front() {
+        assert(length);
+        Node* cur = head->next;
+        delete_node(head);
+        head = cur;
+    }
+
+    void reverse_part(Node* before_begin, int k) { // begin -> the first cubchain , end -> the last cubchain
+        Node* prev = before_begin->next;
+        Node* cur = prev->next;
+        while (--k > 0) {
+            Node* after = cur->next;
+            cur->next = prev;
+
+            prev = cur;
+            cur = after;
+        }
+        before_begin->next->next = cur;
+        before_begin->next = pr
